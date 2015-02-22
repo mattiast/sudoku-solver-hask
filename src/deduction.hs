7db6050cdfc87b1b994a0b527ec40bae341a261b
Matti Astrand <mattiastr@gmail.com>
2015-02-21 20:41:33 -0500

    Some file changes

--- src/deduction.hs
+++
@@ -1,9 +0,0 @@
-module Main where
-import Sudoku
-
-main :: IO ()
-main = do
-    mah <- return . mahisSaturate . mahisParse =<< getContents
-    if mahisNotRR mah
-        then putStr $ mahisRender mah
-        else putStr "CONTRADICTORY" 
